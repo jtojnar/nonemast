29c6e3a3aa3fd07757339c07c885c2888e8eae9e
Jan Tojnar <jtojnar@gmail.com>
2023-03-10 12:57:00 +0100

    Add type stubs for props

---
+++ src/nonemast/window.pyi
@@ -0,0 +1,15 @@
+from gi.repository import Gio
+from gi.repository import Adw
+from gi.repository import Gtk
+from .package_update import PackageUpdate
+
+class UpdateDetails(Gtk.Box):
+    class Props(Gtk.Box.Props):
+        update: PackageUpdate
+        changes_not_reviewed: bool
+    props: Props = ...
+
+class NonemastWindow(Adw.ApplicationWindow):
+    class Props(Adw.ApplicationWindow.Props):
+        updates: Gio.ListStore
+    props: Props = ...
